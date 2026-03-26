1e6869378d7af5c6f8124c23efd72a069a7c0233
Manoj kumar <manojkumardharmavarapu18@gmail.com>
2026-03-26 11:47:33 +0530

    server updated

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://ijiert.com",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/papers", paperRoutes);
